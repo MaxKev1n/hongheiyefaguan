23b6db64741372389e4d81cc5a37c8194e702741
Kev1n <c1048271926@outlook.com>
2020-09-03 12:12:06 +0800

    Add files via upload

---
+++ 1.4.1/headers.h
@@ -0,0 +1,34 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <QMessageBox>
+#include <QSqlDatabase>
+#include <QWidget>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QMainWindow>
+#include <QLabel>
+#include <QTextEdit>
+#include <QTextBrowser>
+#include <QPushButton>
+#include <QDateTime>
+#include <QDialog>
+#include <QLineEdit>
+#include <QTcpSocket>
+#include <QUdpSocket>
+#include <QByteArray>
+#include <QListWidgetItem>
+#include <QListWidget>
+#include <QSqlRecord>
+#include <QVBoxLayout>
+#include <QMap>
+#include <QTcpServer>
+#include <QImage>
+#include <QVector>
+#include <algorithm>
+#include <QPainter>
+#include <QIntValidator>
+
+
+
+#endif // HEADERS_H
---
+++ 1.4.1/main.cpp
@@ -0,0 +1,22 @@
+#include <QApplication>
+#include "loginform.h"
+#include "loginform.cpp"
+#include <welcome.h>
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    welcome wel;
+    wel.setObjectName("dialog");
+    wel.setStyleSheet("QDialog#dialog{border-image: url(://images//wlcomeimage.jpg);}");
+    if (wel.exec() == QDialog::Accepted)
+       {
+        wel.main->show();
+        wel.main->number = wel.inputnum->text().toInt();
+        wel.main->tempnumber = wel.main->number;
+        wel.main->start();
+
+             return app.exec();
+       }
+             else return 0;
+}
